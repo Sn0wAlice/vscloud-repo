44234b412ac7064a897a6bbca6a5d99d696c1565
Alice Snow <snow.alice@pm.me>
2024-12-08 17:39:39 +0100

    add base source code

---
+++ imgs/default/start.sh
@@ -0,0 +1,3 @@
+# Start app
+mkdir -p /config/workspace
+/app/code-server/bin/code-server --auth none --bind-addr 0.0.0.0 --port 8443
---
+++ imgs/default/update.sh
@@ -0,0 +1,16 @@
+# Get the arg, this is the path to programming language
+PROG_LANG=$1
+
+# Check if the arg is empty
+if [ -z "$PROG_LANG" ]; then
+    PROG_LANG="default"
+fi
+
+if [ "$PROG_LANG" = "default" ]; then
+    # nothing
+    echo "Default"
+else
+    echo "Programming language: $PROG_LANG"
+    cat ../$PROG_LANG/Dockerfile >> Dockerfile
+fi
+cat ./Dockerfile.end >> Dockerfile
